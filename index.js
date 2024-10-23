d05d4c11fe37c125391f0a114f57614e99b5819a
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-10-23 14:28:50 -0400

    commit 23-10

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const {PrismaClient} = requite("@prisma/client");
+const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const express = require ("express");
+
 const app = express();
+app.use(express.json());
 
 // Responde a qualquer requisição encaminhada para 
 // /auth/algumaCoisa
-const authRoutes = require("/routes/authRoutes");
+const authRoutes = require("./routes/authRoutes");
 app.use("/auth",authRoutes );
 
 app.listen(8000);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 
 const AuthController = require("../controller/AuthController")
 
-router.post("/cadastro", (req, res) =>{})
-router.post("/login", (req, res) =>{})
+router.post("/cadastro", AuthController.cadastro);
+router.post("/login", AuthController.login);
 
 module.exports = router;

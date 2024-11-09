19051f9451f02b05708134b53cb4075c1d7f596e
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-11-09 14:39:06 -0400

    commit fazendo a operação de cadastro

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,34 +1,69 @@
 const {PrismaClient} = require("@prisma/client");
 const prisma = new PrismaClient();
-const bcryptjs = require("bcryptjs")
+const bcryptjs = require("bcryptjs");
 
 class AuthController{
     static async cadastro(req, res) {
         const{nome, email, password} = req.boby;
+
         if (nome || nome.length < 6) {
-            returnres.statutus(422).json({
+            return res.statutus(422).json({
                 erro: true,
             mernsagem: "O nome deve ter pelo menos 6 caracteres",
             });
         }
-        return res.json({
+
+        if (!email || email.length < 10) {
+            return res.json({
+                erro: true,
+            mernsagem: "O email deve ter pelo menos 10 caracteres",
+            });
+        }
+        if (!password || password.length < 8) {
+            return res.json({
+                erro: true,
+            mernsagem: "A password deve ter pelo menos 10 caracteres",
+            });
+        }
+
+            const existe = await prisma.usuario.count({
+                where: {
+                    email: email
+                },
+            });
+
+            if(existe != 0){
+                return res.json({
+                    erro: true,
+                    mensagem: "Já existe um usuário cadastrado com este e-mail",
+                })
+            }
+
+            const salt = bcryptjs.genSaltSync(8);
+            const hashpassword = bcryptjs(password, salt);
+
+            try{
+            await prisma.usuario.create({
+                data: {
+                    nome: nome,
+                    email: email,
+                    password: hashpassword,
+                    tipo: "cliente",
+                },
+            });
+
+            return res.json({
             erro: false,
-            mensagem: "Usúario cadastrado com sucesso!"
-            token: "3klçjdlfusda9f8as341",
-        });
+            mensagem: "Usuário cadastrado com sucesso!"
+            });
+        } catch (error) {
+            return res.json({
+            erro: true,
+            mensagem: "Ocorreu um erro, tente novamente mais tarde!" + error,
+            });
+        }        
     }
 
-    if (!email || email.length < 10) {
-        retur.res.json({
-            erro: true,
-        mernsagem: "O email deve ter pelo menos 10 caracteres",
-        });
-    }
-    return res.json({
-        erro: false,
-        mensagem: "Usúario cadastrado com sucesso!"
-        token: "3klçjdlfusda9f8as341",
-    });
 
     static async login {
 
---
+++ showuser.js
@@ -0,0 +1,10 @@
+const {PrismaClient} = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function main()
+    {
+        const usuario = await prisma.usuario.flindMany();
+
+        console.log(JSON.stringify(usuario));
+    }
+main()

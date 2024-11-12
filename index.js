e316e3a69350311b49ded0bd4e0ad210a6862f32
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-11-12 09:07:21 -0400

    commit adicionando a parte que faz a ligação entre os dois projetos

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const prisma = require("./prisma/prismaClient");
-
 const express = require ("express");
-
+const cors = require('cors')
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.(cors([
+    origin: "http://localhost:3000",
+    credentials: true
+]))
+
+app.use(express.json());
 // Responde a qualquer requisição encaminhada para 
 // /auth/algumaCoisa
 const authRoutes = require("./routes/authRoutes");
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const AuthController = require("../controller/AuthController")
+const AuthController = require("../controller/AuthController");
 
 router.post("/cadastro", AuthController.cadastro);
 router.post("/login", AuthController.login);

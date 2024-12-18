61ec39d56250c591c69d879f5b7d19a2a578c902
laryssaantunes <larissa.antunes.icloud@gmail.com>
2024-12-18 08:55:28 -0400

    Commit index
    Adicionando as rotas de perfil no index

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-
-iconst prisma = require("./prisma/prismaClient");
-const express = require ("express");
-const cors = require('cors')
+const prisma = require("./prisma/prismaClient");
+const express = require("express");
+const cors = require('cors');
 const app = express();
 const AuthController = require("./controller/AuthController");
+const profileRoutes = require("./routes/ProfileRoutes"); 
+const authRoutes = require("./routes/authRoutes");
 
 app.use(express.json());
-
 app.use(
     cors({
-    origin: "http://localhost:3000",
-    credentials: true
-}))
+        origin: "http://localhost:3000", 
+        credentials: true
+    })
+);
 
 app.use(express.json());
-// Responde a qualquer requisição encaminhada para 
-// /auth/algumaCoisa
-const authRoutes = require("./routes/authRoutes");
-const AuthController = require("./controller/AuthController");
-app.use("/auth",authRoutes );
-// Apenas um exemplo que o professor passou no videio aula 10.
-app.use("/perfil", AuthController.verificaAutenticacao, perfiçRoutes);
-
-app.get("/privado", AuthController.verificaAutenticacao, (req, res)=>
-{
-    res.json(
-        {
-            msg: "Você acessou uma rota restrita!"
-        }
-    );
+app.use("/auth", authRoutes); 
+app.use("/perfil", AuthController.verificaAutenticacao, profileRoutes);  
+app.get("/privado", AuthController.verificaAutenticacao, (req, res) => {
+    res.json({
+        msg: "Você acessou uma rota restrita!"
+    });
 });
-
-app.listen(8000);
+app.listen(8000, () => {
+    console.log("Servidor rodando na porta 8000");
+});

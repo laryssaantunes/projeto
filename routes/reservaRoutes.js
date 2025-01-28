da40613a2d7bc1a91cab251453f9bbf424949e39
laryssaantunes <larissa.antunes.icloud@gmail.com>
2025-01-28 18:55:04 -0400

    Commit 28/01
    Arrumar alguns erros

--- routes/reservaRoutes.js
+++ routes/reservaRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { reservarMesa, listarReservas } = require('../controllers/reservaController');
-const autenticar = require('../middlewares/autenticar');
-
 const router = express.Router();
 
-router.post('/novo', autenticar, reservarMesa);
+const ReservaController = require("../controllers/ReservaController");
+const AuthController = require('../controllers/AuthComtroller');
+
+router.post('/novo', ReservaController.registrarReserva);
 router.get('/', autenticar, listarReservas);
 
 module.exports = router;
